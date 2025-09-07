ece288901409e64feee60ec9d37b0df48665d6fb
I528797 <subhadip.jana@sap.com>
2025-09-07 14:15:06 +0530

    second commit

--- src/main/java/com/bits/jekins_demo/service/StudentService.java
+++ src/main/java/com/bits/jekins_demo/service/StudentService.java
@@ -3,18 +3,21 @@ package com.bits.jekins_demo.service;
 import com.bits.jekins_demo.enity.Student;
 import com.bits.jekins_demo.repository.StudentRepo;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class StudentService {
     @Autowired
     private StudentRepo studentRepo;
 
     public String createStudent(Student student) {
         studentRepo.save(student);
+        log.info("Student created");
         return student.getName();
     }
 
